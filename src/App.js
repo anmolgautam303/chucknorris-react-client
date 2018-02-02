92249579bf8c6daaa536311d1b2eecb6b0b6d1d0
Renam Savio <renamsaviop@gmail.com>
2018-02-02 17:06:28 -0200

    Added AppBar and Collapsible components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Collapsible from 'react-collapsible';
+import TitleBar from './components/TitleBar/TitleBar'
 
-//import './App.scss'
+import './App.scss'
 
 class App extends React.Component {
-  render() {
+render() {
     return (
-      <div className="App">
-        <h1>Test</h1>
-      </div >
+      <div>
+        <div style={{textAlign: "center", paddingBottom: "20px"}}><TitleBar /></div>
+        <div style={{color: "lightBlack", textAlign: "center", fontFamily: 'Heebo, sans-serif'}}><h1>Categories</h1></div>
+        <Collapsible classParentString="App-Collapsible" trigger="Start here" onOpening={()=> alert('opening that shit...')}>
+          <p>This is the collapsible content. It can be any element or React component you like.</p>
+          <p>It can even be another Collapsible component. Check out the next section!</p>
+        </Collapsible>
+        <Collapsible classParentString="App-Collapsible" trigger="Start here">
+          <p>This is the collapsible content. It can be any element or React component you like.</p>
+          <p>It can even be another Collapsible component. Check out the next section!</p>
+        </Collapsible>
+      </div>
     )
   }
 }
